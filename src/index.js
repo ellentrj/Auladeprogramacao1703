ca0753f9b6d41ee5dd9f265db1a9b701d6c7bac1
ellentrj <125470257+ellentrj@users.noreply.github.com>
2023-03-17 10:21:05 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+function imprimirSoma(a, b) {
+  let resultado = a + b;
+  console.log(resultado);
+}
+
+let numero2 = 10;
+let numero3 = 11;
+imprimirSoma(numero2, numero3);
+imprimirSoma(2, 2);
+imprimirSoma(2, 5, 5, 6, 7, 5, 8, 9);
+imprimirSoma();
+
+function soma(a = 5, b = 10) {
+  let resultado = console.log(a + b);
+  return resultado;
+}
+soma();
